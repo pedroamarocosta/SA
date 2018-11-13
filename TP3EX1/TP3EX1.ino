c56add7b62407bf30f7dee3bc51261680912596c
pedroamarocosta <pedro.amaro.costa@gmail.com>
2018-11-13 10:35:19 +0000

    Update TP3&4

--- TP3EX1/TP3EX1.ino
+++ TP3EX1/TP3EX1.ino
@@ -1,26 +1,120 @@
-#define buttonPin 4     // the number of the pushbutton pin
-#define ledPin 13      // the number of the LED pin
+#define BUTTONPIN 4     // the number of the pushbutton pin
+#define LEDPIN 13      // the number of the LED pin
+#define POTPIN A1
+#define RPOTMAX 0.93    //Máximo do potenciometro é 0.93KOHM
+#define VOLTAGE 4.80    //Máximo do potenciometro é 0.93KOHM
 
-bool buttonRead(int pinNumber){
-  return digitalRead(pinNumber);
+bool buttonRead(){
+  return !digitalRead(BUTTONPIN);
 }
 
-void ex1(){
-  buttonRead(buttonPin);
-  if(buttonState == LOW){ //pullup
+bool uartRead(char validChar){
+  char receivedChar;
+  if(Serial.available() > 0){
+      receivedChar = Serial.read();
+      if(receivedChar == validChar){
+        //Serial.println(receivedChar);
+        return true;
+        }
+     }
+   return false;
+}
+
+int potRead10bit(){
+  return analogRead(POTPIN);
+  }
+
+float potReadVolt(){
+  return (VOLTAGE * potRead10bit())/1023.0;
+  }
+
+float potReadR23(){
+  return (RPOTMAX * potReadVolt())/VOLTAGE;
+  }
+
+int potReadPos(){
+  return (100 * potReadR23());
+  }
+
+void EX1(){
+  bool buttonState = buttonRead();
+  if(buttonState == HIGH){ //pullup
     Serial.println("Button is pressed");
   }else{
     Serial.println("Button is not pressed");
   }
-  delay(100);
+}
+
+void EX2(){
+  if(uartRead('A')){
+    //Serial.println("UART function successfull.");
+    }
   }
+  
+void EX3(){
+  int Rpot = potRead10bit();
+  Serial.print(Rpot);
+  Serial.print(", ");
+  }
+  
+void EX4(){
+  float Rpot = potReadVolt();
+  Serial.print(Rpot);
+  Serial.print(", ");
+  }
+
+void EX5(){
+  float Vpot = potReadR23();
+  Serial.print(Vpot);
+  Serial.print(", ");
+  }
+
+void EX6(){
+  float Rpot = potReadPos();
+  Serial.print(Rpot);
+  }
+
+void EX7(){
+  EX3();
+  EX4();
+  EX5();
+  EX6();
+  Serial.println();
+  }
+  
+void EX8(){
+  bool buttonState = buttonRead();
+  if(buttonState == HIGH){ 
+    EX7();
+    digitalWrite(LEDPIN, HIGH);
+    delay(200);
+    digitalWrite(LEDPIN, LOW);
+    delay(200);
+    digitalWrite(LEDPIN, HIGH);
+    delay(200);
+    digitalWrite(LEDPIN, LOW);
+    delay(200);
+    digitalWrite(LEDPIN, HIGH);
+    delay(200);
+    digitalWrite(LEDPIN, LOW);
+    }
+  }
+
 void setup() {
   Serial.begin(115200);
-  pinMode(buttonPin, INPUT_PULLUP);
-  pinMode(ledPin, OUTPUT);
+  pinMode(BUTTONPIN, INPUT_PULLUP);
+  pinMode(LEDPIN, OUTPUT);
+  pinMode(POTPIN, INPUT);
 }
 
 void loop() {
-  ex1();
+  //EX1(); //Função bool buttonRead()
+  //EX2(); //Função bool uartRead(char validChar) 
+  //EX3(); //Função int potRead10bit()
+  //EX4();
+  //EX5();
+  //EX6();
+  //EX7();
+  EX8();
   
 }
