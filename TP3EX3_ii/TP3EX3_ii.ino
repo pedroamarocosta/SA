176cc509f7c0544d33d05b73c4ae3cff657ef54b
pedroamarocosta <pedro.amaro.costa@gmail.com>
2018-12-21 14:45:36 +0000

    Latest Update TP4

--- TP3EX3_ii/TP3EX3_ii.ino
+++ TP3EX3_ii/TP3EX3_ii.ino
@@ -12,54 +12,42 @@
 TP3 tp3(LDRPIN, PIEZOPIN, BUTTONPIN, LEDPIN, POTPIN, LDRMAX, R2, a);
 
 void setup(){
-	Serial.begin(115200);
-//  Serial.println("Imprime o valor original 255 em base hexadecimal: "); 
-//  Serial.println(255, HEX);
-//  Serial.println("Imprime o valor original 255 em base binaria: "); 
-//  Serial.println(255, BIN);
-//  Serial.println("Imprime o valor original 255 em base decimal: "); 
-//  Serial.println(255, DEC);
-//  byte exemplo = 255; //Guardando o valor 255 em 1 byte. Se o valor for maior que 1 byte será truncado
-//  Serial.println("Imprime a variavel byte exemplo nas 3 notacoes."); 
-//  Serial.println(exemplo, HEX);
-//  Serial.println(exemplo, DEC);
-//  Serial.println(exemplo, BIN);
-//  Serial.println("Se o valor nao foi truncado nao ocupa mais que o tipo byte, e portanto apenas 1 byte.");   
+	Serial.begin(115200);  
 }
 
 void loop(){
-//	float rawVal = tp3.potReadVolt();
-//	int val = (int)rawVal+ 1;
-//	switch(val){
-//		case 1:
-//			tone(PIEZOPIN, 2, 500);
-//			delay(500);
-//			noTone(PIEZOPIN);
-//			break;
-//		case 2:
-//			tone(PIEZOPIN, 4, 500);
-//			delay(500);
-//			noTone(PIEZOPIN);
-//			break;
-//		case 3:
-//			tone(PIEZOPIN, 6, 500);
-//			delay(500);
-//			noTone(PIEZOPIN);
-//			break;
-//		case 4:
-//			tone(PIEZOPIN, 8, 500);
-//			delay(500);
-//			noTone(PIEZOPIN);
-//			break;
-//		case 5:
-//			tone(PIEZOPIN, 10, 500);
-//			delay(500);
-//			noTone(PIEZOPIN);
-//			break;
-//    default:
-//      tone(PIEZOPIN, 10, 500);
-//      delay(500);
-//      noTone(PIEZOPIN);
-//      break;
-//	}
+	float rawVal = tp3.potReadVolt();
+	int val = (int)rawVal+ 1;
+	switch(val){
+		case 1:
+			tone(PIEZOPIN, 2, 500);
+			delay(500);
+			noTone(PIEZOPIN);
+			break;
+		case 2:
+			tone(PIEZOPIN, 4, 500);
+			delay(500);
+			noTone(PIEZOPIN);
+			break;
+		case 3:
+			tone(PIEZOPIN, 6, 500);
+			delay(500);
+			noTone(PIEZOPIN);
+			break;
+		case 4:
+			tone(PIEZOPIN, 8, 500);
+			delay(500);
+			noTone(PIEZOPIN);
+			break;
+		case 5:
+			tone(PIEZOPIN, 10, 500);
+			delay(500);
+			noTone(PIEZOPIN);
+			break;
+    default:
+      tone(PIEZOPIN, 10, 500);
+      delay(500);
+      noTone(PIEZOPIN);
+      break;
+	}
 }
--- TP4EX1/TP4EX1.ino
+++ TP4EX1/TP4EX1.ino
@@ -1,8 +1,13 @@
 
+#define ASCEND 0
+#define STAY 1
+#define DESCEND 2
+
 
 void setup() {
-  Serial.begin(115200);
-
+  Serial.begin(1200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(3, OUTPUT);
 }
 
 int tdR(){
@@ -29,8 +34,7 @@ bool timeOver(int intervalo, unsigned long timeStamp){
   }
 }
 
-
-void loop() {
+void EX1(){
   int intervalo = taR();
   Serial.println("The interval measured in function is " + String(intervalo));
   delay(500);
@@ -41,4 +45,96 @@ void loop() {
   }
   Serial.println("The interval has passed!!");
   analogWrite(3, 1);
+}
+
+bool uartRead(char validChar){
+  if(Serial.available()>0){
+    char rChar = Serial.read();
+    if(rChar == validChar){
+      return true;
+    }
+  }else {
+    return false;
+  }
+}
+
+void crescentCycle(){
+  unsigned long timeStamp = millis(); 
+  for(int i=0; i<=80; i++){
+    while(millis() - timeStamp < 50){
+    }
+    int maximum = 0.8 * 255; 
+    int val = map(i, 0, 80, 0, maximum);
+    analogWrite(1, val);
+    Serial.println(val);
+    timeStamp = millis();
+    }
+}
+void constantCycle(){
+  unsigned long timeStamp = millis();
+  int maximum = 255*0.8;
+  for(int i=0; i<100; i++){
+    while(millis() - timeStamp < 50){
+      }
+    analogWrite(1,maximum);
+    Serial.println("Constant!");
+    timeStamp = millis();
+  }
+}
+void descendingCycle(){
+  unsigned long timeStamp = micros();
+  for(int i=80; i>=0; i--){
+    int maximum = 0.8 * 255; 
+    while(micros() - timeStamp < 12500){
+      int val = map(i, 0, 80, 0, maximum);
+      analogWrite(1, val);
+      Serial.println("Descending");
+      timeStamp = micros();
+    }
+  }
+}
+void cycle(){
+  static unsigned long timeStamp = millis();
+  static byte stateSweep = 0;
+  static long intensidade = 0;
+  switch (stateSweep){
+    case ASCEND:
+      //Serial.println("ASCEND");
+      intensidade = 204*(millis()-timeStamp)/4000;
+      Serial.println(intensidade);
+      analogWrite(LED_BUILTIN, intensidade);
+      if(intensidade >=204){
+        stateSweep = STAY;
+        timeStamp = millis();
+      }
+      break;
+     case STAY:
+     //Serial.println("STAY");
+      Serial.println(intensidade);
+      analogWrite(LED_BUILTIN, intensidade);
+      if((millis()-timeStamp) > 5000){
+        stateSweep = DESCEND;
+        timeStamp = millis();
+      }
+      break;
+      case DESCEND:
+      //Serial.println("DESCEND");
+      intensidade = 204-204*(millis()-timeStamp)/1000;
+      Serial.println(intensidade);
+      analogWrite(LED_BUILTIN, intensidade);
+      if(intensidade <= 0){
+        stateSweep = ASCEND;
+        timeStamp = millis();
+      }
+      break;
+  }
+}
+
+void loop() {
+//  char validChar = 'A';
+//  if(uartRead(validChar)){
+//    }
+  digitalWrite(3, HIGH);
+  digitalWrite(3, LOW);
+  cycle();
 }
